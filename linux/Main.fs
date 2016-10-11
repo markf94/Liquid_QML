e48b7ebb8047a96b1c7e9d08b189f9277a1cff14
markf94 <markfingerhuth@protonmail.com>
2016-10-11 19:44:35 +0200

    starting to code the equivalent of the Maria IBM code

--- linux/Main.fs
+++ linux/Main.fs
@@ -29,67 +29,94 @@ module UserSample =
                 )
         (gate theta).Run qs
 
-    //define a quatum function
+    //define a quantum function
     let qfunc (qs:Qubits) =
-        //Hadamard gate
+        //Hadamard on first and fourth qubit
         //H   qs
-
+        //H   qs.[4]
         //rotate qubit by 90 degrees in x direction (Math.PI/2. different direction than H rotation)
-        rotX (Math.PI/4.)   qs //do this to first qubit
+        //rotX (Math.PI/4.)   qs //do this to first qubit
+
+        //extract the individual qubits
+        let q0, q1, q2, q3 = qs.Head, qs.[1], qs.[2], qs.[3]
+
+        //prepare superposition to separate training and input vectors
+        H   [q0]
+        //put m register into superposition
+        H   [q3]
+        //controlled H (q0 control, q1 target)
+        Cgate H [q0;q1]
+        //flip the class label with CNOT (q3 control, q2 target)
+        CNOT [q3;q2]
+
 
         //ENTANGLEMENT
-        for q in qs.Tail do CNOT [qs.Head;q]//select the other qubits but the first one
+        //for q in qs.Tail do CNOT [qs.Head;q]//select the other qubits but the first one
         //apply a CNOT between the head qubit and each other qubit >> results in entanglement
 
         //M does measurement of one qubit (it does the first one in the list)
         M >< qs //bow tie operator applies measurement to all qubits
 
     [<LQD>] //means it can be called from the command line
-    let __UserSample(n:int) =
+    //let __UserSample(n:int) =
+    let __UserSample() =
         //gather statistics
         //array with 2 elements and initialize with 0 >> measuring a qubit and collecting the stats
         let stats   = Array.create 2 0
-        //create state vector containing a single qubit
-        let k       = Ket(n)
-        //create circuit
-        let circ    = Circuit.Compile qfunc k.Qubits
+        let statso  = Array.create 2 0
 
+
+        //create state vector containing a single qubit
+        let k       = Ket(4)
+        let qs = k.Qubits
+
+        //create circuit
+        let circ    = Circuit.Compile qfunc qs
+
+        //Apply(H,qs.[4])
+        //H   qs.[4]
         //Information retrieval
 
-        show "test1:"
+        //show "test1:"
         //output the circuit into the log file
-        circ.Dump()
+        //circ.Dump()
         //Draw it into HTML (H) and Tex (T)
-        circ.RenderHT("Test1")
+        //circ.RenderHT("Test1")
 
         //convolutes the quantum gates >> impossible on an actual quantum computer
         //but leads to a speed up in the classical simulation
         //e.g. collapsing 9 CNOT gates into one matrix >> speed up!
-        let circ    = circ.GrowGates(k)
+        //let circ    = circ.GrowGates(k)
 
-        show "test2:"
+        //show "test2:"
         //output the circuit into the log file
-        circ.Dump()
+        //circ.Dump()
         //Draw it into HTML (H) and Tex (T)
-        circ.RenderHT("Test2")
+        //circ.RenderHT("Test2")
 
 
         for i in 0..9999 do
             //reset the state vector since the measurement will collapse the state vector
-            let qs  = k.Reset(n)
+            //let qa, qb, qc, qd  = a.Reset(), b.Reset(), c.Reset(), d.Reset()
+            let qs = k.Reset()
             //instead of 'qfunc qs' I can run the circuit which does nothing else than running qfunc
             circ.Run qs
+
             //retrieve the bit value of the first qubit in list qs and convert to integer (v)
-            let v   = qs.Head.Bit.v
+            let v   = qs.[0].Bit.v
+            let w   = qs.[3].Bit.v
             //dot between stats and brackets is needed when retrieving objects from a list or array
             stats.[v] <- stats.[v] + 1
+            statso.[w] <- statso.[w] + 1
 
             //Test for entanglement
-            for q in qs.Tail do
-                if q.Bit <> qs.Head.Bit then
-                    failwith "BAD!!!!!"
+            //for q in qs.Tail do
+                //if q.Bit <> qs.Head.Bit then
+                    //failwith "BAD!!!!!"
 
-        show "Measured: 0-%d 1-%d" stats.[0] stats.[1]
+        show "Measured ancilla qubit: 0-%d 1-%d" stats.[0] stats.[1]
+        show "Measured m register: 0-%d 1-%d" statso.[0] statso.[1]
+
 
 module Main =
     open App
